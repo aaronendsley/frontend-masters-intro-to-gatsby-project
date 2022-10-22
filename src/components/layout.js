a7723f6c512b33df29df2473f2af553c132e014c
Aaron Endsley <aaronendsley@gmail.com>
2022-10-21 19:11:50 -0500

    added css modules and worked on the layout component

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Link, useStaticQuery, graphql } from 'gatsby';
 import { Seo } from './seo.js';
 import '../styles/global.css';
+import { header, content } from '../styles/layout.module.css';
 
 
 export default function Layout({ children, title = false, description = false, image = false, path = false }) {
@@ -20,13 +21,13 @@ export default function Layout({ children, title = false, description = false, i
     return (
         <>
             <Seo title={title} description={description} image={image} path={path} />
-            <header>
+            <header className={header}>
                 <Link to="/">{meta.title}</Link>
                 <nav>
                     <Link to="/about">About</Link>
                 </nav>
             </header>
-            <main>
+            <main className={content}>
                 {children}
             </main>
         </>
